9fa587158641a1b4b1c8f8da8562c0a71ce51c03
kyle <jjyu9312@gmail.com>
2024-10-25 10:43:31 +0900

    테스트 코드 추가

--- src/test/java/com/kkw/cafekiosk/AmericanoTest.java
+++ src/test/java/com/kkw/cafekiosk/AmericanoTest.java
@@ -3,6 +3,8 @@ package com.kkw.cafekiosk;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.kkw.cafekiosk.unit.Americano;
+import com.kkw.cafekiosk.unit.CafeKiosk;
+import com.kkw.cafekiosk.unit.Latte;
 import org.junit.jupiter.api.Test;
 
 public class AmericanoTest {
@@ -16,5 +18,37 @@ public class AmericanoTest {
     void add() {
         Americano americano = new Americano();
         assertThat(americano.getName()).isEqualTo("아메리카노");
+
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+    }
+
+    @Test
+    void remove() {
+        Americano americano = new Americano();
+        assertThat(americano.getPrice()).isEqualTo(4000);
+
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(americano);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(0);
+    }
+
+    @Test
+    void clear() {
+        Americano americano = new Americano();
+        assertThat(americano.getPrice()).isEqualTo(4000);
+
+        Latte latte = new Latte();
+        assertThat(latte.getPrice()).isEqualTo(5000);
+
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(0);
     }
 }
