22c5b7c4b96a9faa197b0128db71a650f672033e
kyle <jjyu9312@gmail.com>
2024-10-28 14:41:20 +0900

    spring 디렉토리로 위치 변경

--- src/main/java/com/kkw/cafekiosk/spring/CafeKioskApplication.java
+++ src/main/java/com/kkw/cafekiosk/spring/CafeKioskApplication.java
@@ -1,4 +1,4 @@
-package com.kkw.cafekiosk;
+package com.kkw.cafekiosk.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
