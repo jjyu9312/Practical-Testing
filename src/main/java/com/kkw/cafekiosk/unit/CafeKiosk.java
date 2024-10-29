6823c22193f14517932a788ef591bf27f3e1408c
kyle <jjyu9312@gmail.com>
2024-10-29 10:17:30 +0900

    오버로딩 함수 표시를 위한 주석 추가

--- src/main/java/com/kkw/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/com/kkw/cafekiosk/unit/CafeKiosk.java
@@ -15,13 +15,18 @@ public class CafeKiosk {
 
     private final List<Beverage> beverages = new ArrayList<>();
 
+    // 단순 add 함수
     public void add(Beverage beverage) {
         beverages.add(beverage);
     }
 
+    // add 함수 오버로딩
+    // 한 종류의 음료를 여러 잔 담는 기능
     public void add(Beverage beverage, int count) {
-        if (count <= 0) {
+        if (count < 1) {
             throw new IllegalArgumentException("음료의 수량은 1개 이상이어야 합니다.");
+        } else if (count > 11) {
+            throw new IllegalArgumentException("최대 주문 가능 음료 수량은 10개 입니다.");
         }
 
         for (int i = 0; i < count; i++) {
@@ -31,6 +36,18 @@ public class CafeKiosk {
 
     public void remove(Beverage beverage) {
         beverages.remove(beverage);
+    }
+
+    // remove 함수 오버로딩
+    // 한 종류의 음료를 여러 잔 빼는 기능
+    public void remove(Beverage beverage, int count) {
+        if (count < 1) {
+            throw new IllegalArgumentException("음료의 수량은 1개 이상이어야 합니다.");
+        }
+
+        for (int i = 0; i < count; i++) {
+            beverages.remove(beverage);
+        }
     }
 
     public void clear() {
@@ -54,6 +71,8 @@ public class CafeKiosk {
         return new Order(currentDateTime, beverages);
     }
 
+    // createOrder 함수 오버로딩
+    // 특정 시간을 받아서 주문을 생성하는 기능
     public Order createOrder(LocalDateTime currentDateTime) {
         LocalTime currentTime = currentDateTime.toLocalTime();
 
