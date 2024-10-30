6a922b950fd241be5dc8b9a09a222b25c9ec57e1
kyle <jjyu9312@gmail.com>
2024-10-30 13:14:23 +0900

    불필요한 import 제거

--- src/test/java/com/kkw/cafekiosk/unit/CafeKioskTDDTest.java
+++ src/test/java/com/kkw/cafekiosk/unit/CafeKioskTDDTest.java
@@ -2,19 +2,12 @@
 package com.kkw.cafekiosk.unit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import com.kkw.cafekiosk.unit.order.Order;
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 
 public class CafeKioskTDDTest {
 
     @Test
-    void test() {
-        System.out.println("test");
-    }
+    void test() { System.out.println("test"); }
 
     @Test
     void calculateTotalPrice() {
