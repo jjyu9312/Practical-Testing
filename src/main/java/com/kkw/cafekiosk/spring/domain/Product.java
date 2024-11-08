aa0f903361958309020a65d3e3a54fe62ed332f1
kyle <jjyu9312@gmail.com>
2024-11-08 14:03:59 +0900

    Product entity 생성

---
+++ src/main/java/com/kkw/cafekiosk/spring/domain/Product.java
@@ -0,0 +1,19 @@
+package com.kkw.cafekiosk.spring.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
