e51778d2727f1366f2b20b6f969fb4bb0c2d6d42
kyle <jjyu9312@gmail.com>
2024-11-08 14:04:06 +0900

    Product repository 생성

---
+++ src/main/java/com/kkw/cafekiosk/spring/domain/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.kkw.cafekiosk.spring.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
