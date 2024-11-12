bb3b3b072bec83c5520d45c24d3783efb9fe8eac
kyle <jjyu9312@gmail.com>
2024-11-12 10:28:25 +0900

    주문 목록 조회용 api 생성

--- src/main/java/com/kkw/cafekiosk/spring/ProductController.java
+++ src/main/java/com/kkw/cafekiosk/spring/ProductController.java
@@ -1,8 +1,20 @@
 package com.kkw.cafekiosk.spring;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<ProductsRes> getProducts() {
+        return productService.findAllByProductSellingStatusIn();
+    }
 }
