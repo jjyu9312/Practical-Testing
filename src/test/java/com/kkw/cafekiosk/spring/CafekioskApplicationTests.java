f7d04daba6e8618e91c94280bd1269f4586a4b4c
kyle <jjyu9312@gmail.com>
2024-11-12 10:31:45 +0900

    Profile을 local과 분리하기 위해 test 적용

--- src/test/java/com/kkw/cafekiosk/spring/CafekioskApplicationTests.java
+++ src/test/java/com/kkw/cafekiosk/spring/CafekioskApplicationTests.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CafeKioskApplicationTests {
 
