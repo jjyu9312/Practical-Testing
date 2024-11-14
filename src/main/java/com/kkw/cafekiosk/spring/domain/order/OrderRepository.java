19605c5059827d942ebfe7f5ddaf549351c91c98
kyle <jjyu9312@gmail.com>
2024-11-14 10:39:52 +0900

    Order 관련 도메인 생성

---
+++ src/main/java/com/kkw/cafekiosk/spring/domain/order/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.kkw.cafekiosk.spring.domain.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
