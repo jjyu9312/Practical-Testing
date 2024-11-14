df09edc2fb26bc0dca06d8c12750fbc27f3c5586
kyle <jjyu9312@gmail.com>
2024-11-14 10:39:58 +0900

    OrderProduct 관련 도메인 생성

---
+++ src/main/java/com/kkw/cafekiosk/spring/domain/orderProduct/OrderProductRepository.java
@@ -0,0 +1,7 @@
+package com.kkw.cafekiosk.spring.domain.orderProduct;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+
+}
